5e774eda637c89590775c5c4e01612b3d6f8069e
Jorand <galloujorand@gmail.com>
2024-06-12 18:27:13 +0200

    Backup

---
+++ src/tests/test_1/MILPResults1_pt4_4.sh
@@ -0,0 +1,14 @@
+rostopic pub -l /MILPResults1 diff_drive_robot/MILPResult "RobotID: 1
+Humans: [1]
+GoingStart: [0.0]
+ApproachingStart: [75.0]
+WaitingStart: [95.0]
+ServingStart: [95.0]
+DepotStart: [102.0]
+GoingFinish: [75.0]
+ApproachingFinish: [95.0]
+WaitingFinish: [95.0]
+ServingFinish: [102.0]
+DepotFinish: [264.0]
+FinishedFilling: [1]
+FinishedService: [1]"
