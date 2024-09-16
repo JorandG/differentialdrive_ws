1fe80d64201c6a66c87e1dc6aaae8077caa5a1ad
Jorand <galloujorand@gmail.com>
2024-09-16 18:37:39 +0200

    Updated pause

--- src/Matlab/HRJournal4Hum.m
+++ src/Matlab/HRJournal4Hum.m
@@ -568,7 +568,6 @@ function simulation(ReAll, idx_going_tasks, dist, vel_min, vel_max, inv_vel_min,
                         end
 
                         ReAllSave = ReAll;
-                        pause(0.2);
                        
                         for h=1:num_agents  
                             humanData{h}.StartServing = ReAll.timeS(h+num_agents*3*num_filling_boxes:num_agents:num_agents*3*(num_filling_boxes+1));
@@ -634,7 +633,7 @@ function simulation(ReAll, idx_going_tasks, dist, vel_min, vel_max, inv_vel_min,
                             ReAll = Reallocation1(num_service_tasks, num_tasks, num_agents, num_filling_boxes, num_robots, service_time, timeReall, humanTime_filling, RobotID, ReAll);
                         end
                         ReAllSave = ReAll;
-                        pause(0.2);
+                        
                         humanData{u}.Task = humanData{u}.Task + 1;
                         
                         for h=1:num_agents  
@@ -658,6 +657,7 @@ function simulation(ReAll, idx_going_tasks, dist, vel_min, vel_max, inv_vel_min,
                     end
 
                     display(ReAll, num_robots, num_agents, num_filling_boxes, idx_going_tasks, timeReall);
+                    pause(0.2);
                     %human{u}.confirmModif = 0;
                     % idx_to_consider_r = [];
                     % idx_to_consider_h = [];
