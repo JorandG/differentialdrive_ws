315a2e451fbf2b1f26218875ccb4e05b09da057c
JorandG <91953623+JorandG@users.noreply.github.com>
2024-09-17 15:23:01 +0200

    Update Experiments4.m

--- src/Matlab/Experiments4.m
+++ src/Matlab/Experiments4.m
@@ -28,15 +28,12 @@ if any(timingData >= ReAllSave.timeFh(1)-20) && ~flags(5)
     humanData{1}.StartServing(1) = ReAllSave.timeS(37);
     flags(5) = true;
     send(pub{1}, humanData{1});
-<<<<<<< HEAD
 elseif any(timingData >= ReAllSave.timeFh(2)+60) && ~flags(6)
     finfill = true;
     humanData{2}.FinishFilling(1) = ReAllSave.timeFh(2)+60;
-=======
 elseif any(timingData >= ReAllSave.timeFh(2)+40) && ~flags(6)
     finfill = true;
     humanData{2}.FinishFilling(1) = ReAllSave.timeFh(2)+40;
->>>>>>> b0e92d695d50fe2079c7deeba94b8886f0335ee2
     humanData{2}.ConfirmFilling(1) = 1;
     humanData{2}.StartServing(1) = ReAllSave.timeS(38);
     flags(6) = true;
